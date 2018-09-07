d0a8fe748ee860547138e305c9d2555231852478
khuxkm <khuxkm@tilde.team>
2018-09-07 09:59:43 -0400

    Add >>0 to >>OP conversion

--- client/thread.php
+++ client/thread.php
@@ -7,7 +7,7 @@
 		echo "\t\t<div class='well' id='post{$message["post_id"]}'>".PHP_EOL;
 		echo "\t\t\t<p>&gt;{$message['post_id']} {$username} @ {$time}</p>".PHP_EOL;
 		echo "\t\t\t<pre>";
-		echo $message["body"].PHP_EOL;
+		echo str_replace(">>0",">>OP",$message["body"]).PHP_EOL;
 		echo "</pre>".PHP_EOL;
 		echo "\t\t</div>".PHP_EOL;
 	}
