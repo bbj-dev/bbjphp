20ceaa73c6ef83dcaf7dd96257ce4479bf7b02f1
khuxkm <khuxkm@tilde.team>
2019-10-08 18:13:41 +0000

    Move configuration of server name into client configuration file

--- client/config.php
+++ client/config.php
@@ -5,4 +5,7 @@
 	// Host and Port of BBJ instance
 	define("BBJ_HOST","127.0.0.1");
 	define("BBJ_PORT",7099);
+
+	// Name of BBJ instance
+	define("BBJ_NAME","tildeverse BBJ");
 ?>
--- index.php
+++ index.php
@@ -2,21 +2,20 @@
 	include 'client/config.php';
 	include 'bbj.php';
 	$bbj = new BBJ(BBJ_HOST,BBJ_PORT);
-	$name = "tildeverse BBJ";
 
 	if (isset($_GET["thread_id"])) {
 		$thread = $bbj->thread_load($_GET["thread_id"]);
 		if ($thread["error"]!=null) {
-			$title = "Unknown Thread | ".$name;
+			$title = "Unknown Thread | ".BBJ_NAME;
 			include 'client/header.php';
 			echo "\t\t<p>No such thread exists. <a href='/'>Go home.</a></p>";
 			include 'client/footer.php';
 			die();
 		} else {
-			$title = $thread["data"]["title"]." by ".$thread["usermap"][$thread["data"]["author"]]["user_name"]." | ".$name;
+			$title = $thread["data"]["title"]." by ".$thread["usermap"][$thread["data"]["author"]]["user_name"]." | ".BBJ_NAME;
 		}
 	} else {
-		$title = $name;
+		$title = BBJ_NAME;
 	}
 
 	include 'client/header.php';
