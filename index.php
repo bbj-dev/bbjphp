ed3bcb40edbbf17bcad5e454684c9dd2aeb9c0db
khuxkm fbexl <khuxkm@ttm.sh>
2019-10-08 11:25:31 -0400

    Make name configurable

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 	include 'client/config.php';
 	include 'bbj.php';
 	$bbj = new BBJ(BBJ_HOST,BBJ_PORT);
+	$name = "tildeverse BBJ";
 
 	if (isset($_GET["thread_id"])) {
 		$thread = $bbj->thread_load($_GET["thread_id"]);
 		if ($thread["error"]!=null) {
-			$title = "Unknown Thread | ~team BBJ";
+			$title = "Unknown Thread | ".$name;
 			include 'client/header.php';
 			echo "\t\t<p>No such thread exists. <a href='/'>Go home.</a></p>";
 			include 'client/footer.php';
 			die();
 		} else {
-			$title = $thread["data"]["title"]." by ".$thread["usermap"][$thread["data"]["author"]]["user_name"]." | ~team BBJ";
+			$title = $thread["data"]["title"]." by ".$thread["usermap"][$thread["data"]["author"]]["user_name"]." | ".$name;
 		}
 	} else {
-		$title = "~team BBJ";
+		$title = $name;
 	}
 
 	include 'client/header.php';
